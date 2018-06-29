bc53654dfc076feaeb900ebd4d12e12b5daf5e3a
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-06-29 17:05:10 +0100

    Add server and server test fixture

---
+++ dtool_http/server.py
@@ -0,0 +1,64 @@
+"""Basic Dtool HTTP server."""
+
+import os
+import json
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+import dtoolcore
+from dtoolcore.utils import urlunparse
+
+
+class DtoolHTTPRequestHandler(SimpleHTTPRequestHandler):
+
+    def generate_url(self, suffix):
+        url_base_path = os.path.dirname(self.path)
+        netloc = "{}:{}".format(*self.server.server_address)
+        return urlunparse((
+            "http",
+            netloc,
+            url_base_path + "/" + suffix,
+            "", "", ""))
+
+    def generate_item_urls(self):
+        item_urls = {}
+        for i in self.dataset.identifiers:
+            relpath = self.dataset.item_properties(i)["relpath"]
+            url = self.generate_url("data/" + relpath)
+            item_urls[i] = url
+        return item_urls
+
+    def generate_overlay_urls(self):
+        overlays = {}
+        for o in self.dataset.list_overlay_names():
+            url = self.generate_url(".dtool/overlays/{}.json".format(o))
+            overlays[o] = url
+        return overlays
+
+    def generate_http_manifest(self):
+        base_path = os.path.dirname(self.translate_path(self.path))
+        self.dataset = dtoolcore.DataSet.from_uri(base_path)
+        admin_metadata_fpath = os.path.join(base_path, ".dtool", "dtool")
+        with open(admin_metadata_fpath) as fh:
+            admin_metadata = json.load(fh)
+
+        http_manifest = {
+            "admin_metadata": admin_metadata,
+            "manifest_url": self.generate_url(".dtool/manifest.json"),
+            "readme_url": self.generate_url("README.yml"),
+            "overlays": self.generate_overlay_urls(),
+            "item_urls": self.generate_item_urls()
+        }
+        return bytes(json.dumps(http_manifest), "utf-8")
+
+    def do_GET(self):
+        if self.path.endswith("http_manifest.json"):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(self.generate_http_manifest())
+        else:
+            super(DtoolHTTPRequestHandler, self).do_GET()
+
+
+class DtoolHTTPServer(HTTPServer):
+    pass
--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,8 @@
 import os
 import shutil
 import tempfile
+from urllib.parse import urlunparse
+import threading
 
 import dtoolcore
 import pytest
@@ -11,19 +13,12 @@ _HERE = os.path.dirname(__file__)
 _DATA = os.path.join(_HERE, "data")
 
 
-@pytest.fixture
-def tmp_disk_dataset_uri(request):
-    d = tempfile.mkdtemp()
-
-    @request.addfinalizer
-    def teardown():
-        shutil.rmtree(d)
-
+def create_tmp_dataset(directory):
     admin_metadata = dtoolcore.generate_admin_metadata("tmp_dataset")
 
     proto_dataset = dtoolcore.generate_proto_dataset(
         admin_metadata=admin_metadata,
-        base_uri=d,
+        base_uri=directory,
         config_path=None)
 
     proto_dataset.create()
@@ -35,3 +30,54 @@ def tmp_disk_dataset_uri(request):
     proto_dataset.freeze()
 
     return proto_dataset.uri
+
+
+@pytest.fixture
+def tmp_disk_dataset_uri(request):
+    d = tempfile.mkdtemp()
+
+    @request.addfinalizer
+    def teardown():
+        shutil.rmtree(d)
+
+    return create_tmp_dataset(d)
+
+
+@pytest.fixture(scope="session")
+def tmp_dtool_server(request):
+
+    d = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(d)
+
+    uri = create_tmp_dataset(d)
+    dataset = dtoolcore.DataSet.from_uri(uri)
+
+
+    server_address = ("localhost", 8081)
+
+    from dtool_http.server import DtoolHTTPServer, DtoolHTTPRequestHandler
+    httpd = DtoolHTTPServer(server_address, DtoolHTTPRequestHandler)
+
+    def start_server():
+        print("start dtool http server")
+        httpd.serve_forever()
+
+    t = threading.Thread(target=start_server)
+    t.start()
+
+
+    @request.addfinalizer
+    def teardown():
+        httpd.shutdown()
+        print("stopping dtool http server")
+        os.chdir(curdir)
+        shutil.rmtree(d)
+
+    netloc = "{}:{}".format(*server_address)
+    path = dataset.name
+    return urlunparse((
+        "http",
+        netloc,
+        path,
+        "", "", ""))
